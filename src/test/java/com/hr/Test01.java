0d0414d7d8b89e20211b1f404211f2a1ca2091ea
fange518 <60872842+fange518@users.noreply.github.com>
2021-07-08 11:05:43 +0800

    spring依赖注入

--- src/test/java/com/hr/Test01.java
+++ src/test/java/com/hr/Test01.java
@@ -3,17 +3,25 @@ package com.hr;
 import com.hr.entity.User;
 import com.hr.service.UserService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
 public class Test01 {
 
+    @Autowired
+    private UserService userService;
     @Test //无参，无返回值
     public void test01(){
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserService userService = (UserService) context.getBean("userService");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        UserService userService = (UserService) context.getBean("userService");
         List<User> users = userService.queryUsers();
         users.forEach(System.out::println);
     }
